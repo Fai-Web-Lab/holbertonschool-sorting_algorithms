13eb8340c49e5e045057a995c59367fca1d91043
Fai-Web-Lab <fai.alsharekh@gmail.com>
2025-12-18 01:22:59 +0300

    2nd task in the Sorting algorithms & Big O project

--- sort.h
+++ sort.h
@@ -3,8 +3,25 @@
 
 #include <stddef.h>
 
-void bubble_sort(int *array, size_t size);
+/**
+	* struct listint_s - Doubly linked list node
+	* @n: Integer stored in the node
+	* @prev: Pointer to the previous node
+	* @next: Pointer to the next node
+	*
+	* Description: Doubly linked list node structure for sorting algorithms.
+	*/
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
 void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 
 #endif
